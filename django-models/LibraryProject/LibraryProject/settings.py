7daab2cac5c9c140a2f5ccc898bed1fa714d021b
petergodwin <chinegbomkpapeter@gmail.com>
2025-03-02 21:53:42 +0100

    Implementing User Authentication in Django

--- django-models/LibraryProject/LibraryProject/settings.py
+++ django-models/LibraryProject/LibraryProject/settings.py
@@ -41,6 +41,13 @@ INSTALLED_APPS = [
     'relationship_app'
 ]
 
+AUTHENTICATION_BACKENDS = [
+  'django.contrib.auth.backends.ModelBackend',
+  ]
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/login/'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,13 @@
 from django.urls import include, path
 from .views import list_books, LibraryDetailView
+from .views import login_view, logout_view, register_view
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
   path("books/", list_books, name="list_books"),
   path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
-  path("", include("relationship_app.urls"))
+  path("", include("relationship_app.urls")),
+  path('login/', login_view, name='login'),
+  path('logout/', logout_view, name='logout'),
+  path('register/', register_view, name='register'),
 ]

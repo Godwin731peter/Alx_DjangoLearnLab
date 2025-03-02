e632f93d9621b0dfb13f10dba213a9260b5e5728
petergodwin <chinegbomkpapeter@gmail.com>
2025-03-02 22:00:15 +0100

    fixeda bug

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic.detail import DetailView
 from django.contrib.auth import login, logout, authenticate
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import UserCreationForm
 from .models import Book
 from .models import Library
 

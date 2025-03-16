1756a4f747c6b616076fbd93c1c7f1b85e95396f
petergodwin <chinegbomkpapeter@gmail.com>
2025-03-16 05:23:51 +0100

    added ExampleForm to te forms.py

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -3,3 +3,8 @@ from .models import Book
 
 class BookSearchForm(forms.Form):
     query = forms.CharField(max_length=100, required=False)
+
+class ExampleForm(forms.ModelForm):
+    class Meta:
+        model = Book  # Replace with the correct model
+        fields = ['title', 'author', 'published_date']
